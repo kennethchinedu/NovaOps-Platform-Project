e01f0af7ce05b0f701cf30fb8d7cf1691b89a86d
kennethchinedu <anamsken60@gmail.com>
2025-10-28 00:12:23 +0100

    setting up rancher and argocd resources

--- infra/modules/utils/main.tf
+++ infra/modules/utils/main.tf
@@ -30,35 +30,34 @@ resource "helm_release" "cert_manager" {
   
 }
 
-# Rancher
-resource "helm_release" "rancher" {
-  name             = "rancher"
-  repository       = "https://releases.rancher.com/server-charts/latest"
-  chart            = "rancher"
-  namespace        = "cattle-system"
+#Argocd
+resource "helm_release" "argocd" {
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
   create_namespace = true
-
-  set =[ {
-    name  = "hostname"
-    value = ""
-  },
-  #Disabling default service and setting up ours
-  {
-    name  = "service.enabled"
-    value = "false"
-    
-  },
- {
-    name = "ingress.enabled"
-    value = "false"
-  },
-  {
-    name  = "service.ports.http"
-    value = "8080"
-  },
- {
-    name  = "service.ports.https"
-    value = "8443"
-  }
-  ]
 }
+
+
+# # Rancher
+# resource "helm_release" "rancher" {
+#   name             = "rancher"
+#   repository       = "https://releases.rancher.com/server-charts/latest"
+#   chart            = "rancher"
+#   namespace        = "cattle-system"
+#   create_namespace = true
+
+#   set =[ {
+#     name  = "hostname"
+#     value = "acb8d0f2e478547e0a0b5e14c4b4f72c-1734230420.us-east-1.elb.amazonaws.com"
+#   }
+#   , {
+#     name  = "ingress.tls.source"
+#     value = "rancher"
+#     }
+#   ]
+
+
+#   depends_on = [ helm_release.cert_manager ]
+# }
